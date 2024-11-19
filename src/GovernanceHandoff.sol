649312b4ca219680b04cfb5a90748125725080f6
Nishaad <nknavkal@gmail.com>
2024-11-19 14:54:08 -0500

    temp

---
+++ src/GovernanceHandoff.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Proposal} from "currency-1.5/governance/community/proposals/Proposal.sol";
+import {ECOx} from "currency-1.5/currency/ECOx.sol";
+import {Policy} from "currency-1.5/policy/Policy.sol";
+// import {TrustedNodes} from "currency-1.5/governance/monetary/TrustedNodes.sol";
+
+
+contract TrusteePayout is Proposal {
+
+    ECOx public constant ECOX = ECOx('0xcccD1Ba9f7acD6117834E0D28F25645dECb1736a');
+
+    address public constant EXCHANGE = '0xa6285d7a469f6d7c46BF24022e309429c9d3f4Ba';
+
+    address public immutable governorMultisig; 
+
+    constructor(address _multisig) {
+        governorMultisig = _multisig;
+    }
+
+    function name() public pure virtual override returns (string memory) {
+        return "Governance Transition";
+    }
+
+    function description()
+        public
+        pure
+        virtual
+        override
+        returns (string memory)
+    {
+        return
+            "moves governance to a multisig, also removes ECO/ECOx mint/burn permissions from both the policy contract and ECOxExchange";
+    }
+
+    /** A URL where more details can be found.
+     */
+    function url() public pure override returns (string memory) {
+        return
+            "";
+    }
+
+    function enacted(address _self) public virtual override{
+
+        // Update the governor from CommunityGovernance contract to the multisig
+        Policy(address(this)).updateGovernor(governorMultisig);
+
+        // revoke mint and burn permissions from the policy contract
+        ECOX.updateMinters(address(this), false);
+        ECOX.updateBurners(address(this), false);
+    }
+
+}  
